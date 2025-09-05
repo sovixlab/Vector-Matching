a17533c78a30ed44f45a4b2ab593922c6a09de0d
sovixlab <erwin@sovix.nl>
2025-09-05 16:02:23 +0200

    "Fix migratie conflict: verwijder dubbele 0002 migratie"

--- vector_matching_app/migrations/0005_make_fields_nullable.py
+++ vector_matching_app/migrations/0005_make_fields_nullable.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('vector_matching_app', '0001_initial'),
+        ('vector_matching_app', '0004_candidate_extended'),
     ]
 
     operations = [
